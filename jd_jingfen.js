d7c2ae943f5b8a7cb953f20d6740b5369238bcbf
wisz2021 <wei09mozzie@gmail.com>
2021-12-28 12:35:07 +0800

    升级v2版本，可转京喜APP商品链接

--- jd_jingfen.js
+++ jd_jingfen.js
@@ -5,7 +5,7 @@
 可在 https://union.jd.com/index 平台上面获取，详情如下：
 https://union.jd.com/user 获取联盟id（unionId），
 https://union.jd.com/manager/promotionSite 获取推广位id（positionId）
-其中：appKey（可选）和appSecret（可选）可在https://www.dgrlm.com/open/index申请
+其中：appKey（可选）和appSecret（可选）可在 https://www.dgrlm.com/open/index 申请
 省时省力一键转链接口：https://www.dgrlm.com/qcypopen/open/v1/qcSmartChain（调取量30万/天，且用且珍惜！）
 0 6,18 * * * jd_jingfen.js
  */
@@ -13,6 +13,7 @@ let unionId = 2011649855  // 联盟id
 let positionId = 3004050005  // 推广位id
 let appKey = '80CD670689F33574860091B36139A527';
 let appSecret = '897050AB1CEA5F4626838640B323E5280D66CDFEB38F0F4C6E7E107E1471E86D';
+const version = 'v2';// 接口版本：v1（旧版本），v2（新版本），默认为v1版本，建议使用v2版本
 const $ = new Env('京东购物车商品转链');
 // md5
 !function(n){function t(n,t){var r=(65535&n)+(65535&t);return(n>>16)+(t>>16)+(r>>16)<<16|65535&r}function r(n,t){return n<<t|n>>>32-t}function e(n,e,o,u,c,f){return t(r(t(t(e,n),t(u,f)),c),o)}function o(n,t,r,o,u,c,f){return e(t&r|~t&o,n,t,u,c,f)}function u(n,t,r,o,u,c,f){return e(t&o|r&~o,n,t,u,c,f)}function c(n,t,r,o,u,c,f){return e(t^r^o,n,t,u,c,f)}function f(n,t,r,o,u,c,f){return e(r^(t|~o),n,t,u,c,f)}function i(n,r){n[r>>5]|=128<<r%32,n[14+(r+64>>>9<<4)]=r;var e,i,a,d,h,l=1732584193,g=-271733879,v=-1732584194,m=271733878;for(e=0;e<n.length;e+=16){i=l,a=g,d=v,h=m,g=f(g=f(g=f(g=f(g=c(g=c(g=c(g=c(g=u(g=u(g=u(g=u(g=o(g=o(g=o(g=o(g,v=o(v,m=o(m,l=o(l,g,v,m,n[e],7,-680876936),g,v,n[e+1],12,-389564586),l,g,n[e+2],17,606105819),m,l,n[e+3],22,-1044525330),v=o(v,m=o(m,l=o(l,g,v,m,n[e+4],7,-176418897),g,v,n[e+5],12,1200080426),l,g,n[e+6],17,-1473231341),m,l,n[e+7],22,-45705983),v=o(v,m=o(m,l=o(l,g,v,m,n[e+8],7,1770035416),g,v,n[e+9],12,-1958414417),l,g,n[e+10],17,-42063),m,l,n[e+11],22,-1990404162),v=o(v,m=o(m,l=o(l,g,v,m,n[e+12],7,1804603682),g,v,n[e+13],12,-40341101),l,g,n[e+14],17,-1502002290),m,l,n[e+15],22,1236535329),v=u(v,m=u(m,l=u(l,g,v,m,n[e+1],5,-165796510),g,v,n[e+6],9,-1069501632),l,g,n[e+11],14,643717713),m,l,n[e],20,-373897302),v=u(v,m=u(m,l=u(l,g,v,m,n[e+5],5,-701558691),g,v,n[e+10],9,38016083),l,g,n[e+15],14,-660478335),m,l,n[e+4],20,-405537848),v=u(v,m=u(m,l=u(l,g,v,m,n[e+9],5,568446438),g,v,n[e+14],9,-1019803690),l,g,n[e+3],14,-187363961),m,l,n[e+8],20,1163531501),v=u(v,m=u(m,l=u(l,g,v,m,n[e+13],5,-1444681467),g,v,n[e+2],9,-51403784),l,g,n[e+7],14,1735328473),m,l,n[e+12],20,-1926607734),v=c(v,m=c(m,l=c(l,g,v,m,n[e+5],4,-378558),g,v,n[e+8],11,-2022574463),l,g,n[e+11],16,1839030562),m,l,n[e+14],23,-35309556),v=c(v,m=c(m,l=c(l,g,v,m,n[e+1],4,-1530992060),g,v,n[e+4],11,1272893353),l,g,n[e+7],16,-155497632),m,l,n[e+10],23,-1094730640),v=c(v,m=c(m,l=c(l,g,v,m,n[e+13],4,681279174),g,v,n[e],11,-358537222),l,g,n[e+3],16,-722521979),m,l,n[e+6],23,76029189),v=c(v,m=c(m,l=c(l,g,v,m,n[e+9],4,-640364487),g,v,n[e+12],11,-421815835),l,g,n[e+15],16,530742520),m,l,n[e+2],23,-995338651),v=f(v,m=f(m,l=f(l,g,v,m,n[e],6,-198630844),g,v,n[e+7],10,1126891415),l,g,n[e+14],15,-1416354905),m,l,n[e+5],21,-57434055),v=f(v,m=f(m,l=f(l,g,v,m,n[e+12],6,1700485571),g,v,n[e+3],10,-1894986606),l,g,n[e+10],15,-1051523),m,l,n[e+1],21,-2054922799),v=f(v,m=f(m,l=f(l,g,v,m,n[e+8],6,1873313359),g,v,n[e+15],10,-30611744),l,g,n[e+6],15,-1560198380),m,l,n[e+13],21,1309151649),v=f(v,m=f(m,l=f(l,g,v,m,n[e+4],6,-145523070),g,v,n[e+11],10,-1120210379),l,g,n[e+2],15,718787259),m,l,n[e+9],21,-343485551),l=t(l,i),g=t(g,a),v=t(v,d),m=t(m,h)}return[l,g,v,m]}function a(n){var t,r="",e=32*n.length;for(t=0;t<e;t+=8){r+=String.fromCharCode(n[t>>5]>>>t%32&255)}return r}function d(n){var t,r=[];for(r[(n.length>>2)-1]=void 0,t=0;t<r.length;t+=1){r[t]=0}var e=8*n.length;for(t=0;t<e;t+=8){r[t>>5]|=(255&n.charCodeAt(t/8))<<t%32}return r}function h(n){return a(i(d(n),8*n.length))}function l(n,t){var r,e,o=d(n),u=[],c=[];for(u[15]=c[15]=void 0,o.length>16&&(o=i(o,8*n.length)),r=0;r<16;r+=1){u[r]=909522486^o[r],c[r]=1549556828^o[r]}return e=i(u.concat(d(t)),512+8*t.length),a(i(c.concat(e),640))}function g(n){var t,r,e="";for(r=0;r<n.length;r+=1){t=n.charCodeAt(r),e+="0123456789abcdef".charAt(t>>>4&15)+"0123456789abcdef".charAt(15&t)}return e}function v(n){return unescape(encodeURIComponent(n))}function m(n){return h(v(n))}function p(n){return g(m(n))}function s(n,t){return l(v(n),v(t))}function C(n,t){return g(s(n,t))}function A(n,t,r){return t?r?s(t,n):C(t,n):r?m(n):p(n)}$.md5=A}(this);
@@ -151,6 +152,7 @@ async function getCartUrls() {
     const options = {
       headers: {
         'Cookie': cookie,
+        "User-Agent": "jdpingou"
       },
       url: 'https://p.m.jd.com/cart/cart.action?fromnav=1&sceneval=2&jxsid=',
       timeout: 10000
@@ -162,7 +164,7 @@ async function getCartUrls() {
           console.log(`提取购物车商品列表请求失败：${$.toStr(err)}\n`)
         } else {
           console.log("提取购物车商品列表请求成功：")
-          const pattern = /skuItemUrl":"(.*?)\?/g
+          const pattern = /skuItemUrl":"(.*?)\&_fd/g
           const items = matchall(pattern, data)
           if (pattern.test(data)) {
             for (let item of items) {
@@ -191,14 +193,16 @@ async function changeLinks(urls) {
     const data = await qcSmartChain(url, sign, timestamp);
     await $.wait(parseInt(Math.random() * 500, 10))
     try {
-      if (data.skuInfos[0].skuUrl.includes('u.jd.com')) {
-        finalUrls.push(data.skuInfos[0].skuUrl)
-      } else {
-        finalUrls.push(data['info'].replace(/\r\n/, ''))
+      if (data) {
+        if (data.skuInfos[0].skuUrl.includes('u.jd.com')) {
+          finalUrls.push(data.skuInfos[0].skuUrl)
+        } else {
+          finalUrls.push(data['info'].replace(/\r\n/, ''))
+        }
+        console.log('购物车商品名称：', data.skuInfos[0].skuName)
       }
-      console.log('购物车商品名称：', data.skuInfos[0].skuName)
     } catch (err) {
-      console.log(`\n当前商品：${data['info'].replace(/\r\n/, '')}不在推广中\n`);
+      console.log(`当前商品：${data['info'].replace(/\r\n/, '')}不在推广中`);
     }
   }
   return finalUrls
@@ -212,7 +216,7 @@ function qcSmartChain(url, sign, timestamp) {
       "copywriting": url,
       "unionId": unionId,
       "positionId": positionId,
-      "version": 'v1'
+      version
     }
     const options = {
       body: $.toStr(body),
@@ -233,7 +237,8 @@ function qcSmartChain(url, sign, timestamp) {
             if (data['data']['info'].includes('u.jd.com')) console.log('短链：', data['data']['info'].replace(/\r\n/, ''))
             resolve(data['data']);
           } else {
-            console.log(`qcSmartChain请求失败：${$.toStr(data)}\n`);
+            console.log(`${url}： ${$.toStr(data)}`);
+            resolve();
           }
         }
       } catch (e) {
@@ -249,7 +254,6 @@ function qcSmartChain(url, sign, timestamp) {
 copywriting：待转链文案，此处仅需要原商品链接即可
  */
 function makeSign(copywriting = '', timestamp) {
-  const version = 'v1'  // 版本，建议v1
   let signPre = `appKey${appKey}copywriting${copywriting}positionId${positionId}timestamp${timestamp}unionId${unionId}version${version}`  // ascii值排序
   let signStr = appSecret + signPre + appSecret
   return $.md5(signStr).toUpperCase()
