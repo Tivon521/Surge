8beae969b8d5e51a02a42fa75597990ca5b4df69
123 <123@123.com>
2022-01-02 00:04:09 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -32,8 +32,6 @@
 35 */1 * * * node /scripts/jd_cow_mengniu.js >> /scripts/logs/jd_cow_mengniu.log 2>&1
 # 清风云养树
 15 4,15 * * * node /scripts/jd_tree.js >> /scripts/logs/jd_tree.log 2>&1
-#小魔方（活动时间：2021年12月27日 – 1月1日24:00）
-1 0,19,22 * * * node /scripts/jd_desire.js >> /scripts/logs/jd_desire.log 2>&1
 ##############长期活动##############
 # 签到
 7 0,17 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
