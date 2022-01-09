e135923d65aee5e7bb07523c8b939773217a1742
123 <123@123.com>
2022-01-09 19:09:07 +0800

    年货红包🧧

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -36,6 +36,8 @@
 0 20 9,24 1 * node /scripts/jd_hby.js >> /scripts/logs/jd_hby.log 2>&1
 #城城分现金
 0 0-23/1 * * * node /scripts/jd_city.js >> /scripts/logs/jd_city.log 2>&1
+#年货红包🧧
+0 0,20,23 * * * node /scripts/jd_nianRed.js >> /scripts/logs/jd_nianRed.log 2>&1
 ##############长期活动##############
 # 签到
 7 0,17 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
