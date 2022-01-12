af6db93f3f74cdc5b43e04e843b7ea92462173b2
123 <123@123.com>
2022-01-12 08:17:26 +0800

    20京豆

--- jd_flsSign.js
+++ jd_flsSign.js
@@ -37,6 +37,10 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
   }
   try {
     let promiseArr = null;
+    
+    promiseArr = cookiesArr.map((ck, index) => getActInfo(ck, index, 'https://prodev.m.jd.com/mall/active/297D8EdMPuCqPDERRYdN2D1e39oe/index.html'));
+    await Promise.all(promiseArr);
+    
     promiseArr = cookiesArr.map((ck, index) => getActInfo(ck, index, 'https://pro.m.jd.com/mall/active/4Vh5ybVr98nfJgros5GwvXbmTUpg/index.html'));
     await Promise.all(promiseArr);
 
